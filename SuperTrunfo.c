238efc150db641cb9657657a8f1c411e0664a586
SirAnonyo <papapyro72@gmail.com>
2025-02-25 14:42:40 +0000

    Refiz o codigo e inclui o processo para fazer a segunda carta

---
+++ SuperTrunfo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    int Populaçâo;
+    int Pontos;
+    float Área;
+    float PIB;
+    char Estado[20];
+    char Cidade[50];
+    char Carta[4];
+
+    printf("digite o nome do estado\n");
+    scanf("%s" &Estado);
+
+    printf("digite o nome da cidade\n");
+    scanf("%s" &Cidade);
+
+    printf("digite a área da cidade em km\n");
+    scanf("%f" &Área);
+
+    printf("digite a população da cidade\n");
+    scanf("%f" &População);
+
+    printf("digite o PIB per capita da cidade")
+}
